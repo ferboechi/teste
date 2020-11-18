3d5352991f06b3d8372983e651dda4f189b97190
Fernando Boechi <ferboechi@live.com>
2020-11-18 20:40:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({ teste: "teste"});
 })
 
 app.listen(3000, function() {
